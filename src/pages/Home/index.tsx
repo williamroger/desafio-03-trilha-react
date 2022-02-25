e2e5c9caf1503ef031671d68cca358fa4f2c3f9a
William Roger <williamroger.frontend@gmail.com>
2022-02-25 19:21:47 -0300

    finalizado componente Header + ajustes no componente Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,10 +26,9 @@ const Home = (): JSX.Element => {
   const { addProduct, cart } = useCart();
 
   const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    return {
-      [product.id]: product.amount,
-    }
-  }, {} as CartItemsAmount)
+    sumAmount[product.id] = product.amount;
+    return sumAmount;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
